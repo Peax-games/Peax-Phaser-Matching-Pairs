303752ff91da0349c101d49bfacdf8f2a5ab617c
Christopher Fox <cfox1290@gmail.com>
2018-03-01 17:30:15 -0700

    doesnt work right now

--- src/Game/states/boot.js
+++ src/Game/states/boot.js
@@ -1,7 +1,7 @@
 export default function bootState(){
     return{
         create: function(){
-            this.game.physics.startSystem(window.Phaser.Physics.ARCADE);
+            // this.game.physics.startSystem(window.Phaser.Physics.ARCADE);
             this.game.state.start('load');
         }
     }
--- src/Game/states/load.js
+++ src/Game/states/load.js
@@ -1,5 +1,19 @@
 export default function loadState(){
     return{
-        
+        preload: function(){
+            var loadingLabel = this.game.add.text(80, 150, 'loading...',
+            { font: '30px Courier', fill: '#fff' });
+
+        this.game.scale.scaleMode = window.Phaser.ScaleManager.NO_SCALE;
+        this.game.scale.pageAlignHorizontally = true;
+        this.game.scale.pageAlignVertically = true;
+        //game.stage.backgroundColor = '#eee';
+        this.game.load.tilemap('matching', 'img/tiles.json', null, window.Phaser.Tilemap.TILED_JSON);
+        this.game.load.image('tiles', 'img/tiles.png');//, 100, 100, -1, 1, 1);    
+        },
+        create: function(){
+            this.game.state.start('play')
+        }
+
     }
 }
